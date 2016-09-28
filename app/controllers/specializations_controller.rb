d103364885034d032b47ebab9d76c2fb26d31656
Tylor Dodge <tylor.dodge1@gmail.com>
2016-09-28 12:37:17 -0400

    now able to add specialties to users

--- app/controllers/specializations_controller.rb
+++ app/controllers/specializations_controller.rb
@@ -1,6 +1,15 @@
 class SpecializationsController < ApplicationController
 
   def create
+    @specialization = current_user.specializations.build(:specialty_id => params[:specialty_id])
+    if @specialization.save
+      flash[:notice] = "Added specialty"
+      redirect_to root_url
+
+    else
+      flash[:notice] = "error"
+      redirect_to root_url
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     collection { post :import}
   end
   resources :specialties
+  resources :specializations
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   # The priority is based upon order of creation: first created -> highest priority.
